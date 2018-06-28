4a09f069d70c51dec3fd927061a0785f864161cc
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-28 13:24:57 -0700

    Checkpoint1

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -18,8 +18,11 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
-
+	String animal = JOptionPane.showInputDialog("What animal do you want?");
 	/* 2. Make it so that the user can keep entering new animals. */
+	if (animal.equalsIgnoreCase("cow")) {
+		playMoo();
+	}
 }
 
 void playMoo() {
