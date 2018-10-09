aa8ba9543b17720f7df96fcad89b2dd6e2adbb8b
96752 <96752@0338S-S-R0B6H0D.sdcs.local>
2018-10-09 14:25:35 -0700

    NumberGuessingGame

---
+++ src/random/NumberGuessingGame.java
@@ -0,0 +1,39 @@
+package random;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class NumberGuessingGame {
+	public static void main(String[] args) {
+	
+	Random gen = new Random ();
+		int r = 0;
+		Random factory = new Random();
+			r =factory.nextInt(3);
+	System.out.print(r);
+	int answer = 0;
+	if(answer==r);
+	String BobbyTarantino = JOptionPane.showInputDialog("Guess the number");
+	
+	
+	
+	
+	
+}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
