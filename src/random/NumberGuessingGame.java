de965721f1b85f88cee91661d3bbf56c7bc19a57
96752 <96752@0338S-S-R0B6H0D.sdcs.local>
2018-10-16 14:20:46 -0700

    NumberGuessingGame

--- src/random/NumberGuessingGame.java
+++ src/random/NumberGuessingGame.java
@@ -5,35 +5,23 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class NumberGuessingGame {
-	public static void main(String[] args) {
+public static void main(String[] args) {
 	
-	Random gen = new Random ();
-		int r = 0;
-		Random factory = new Random();
-			r =factory.nextInt(3);
-	System.out.print(r);
-	int answer = 0;
-	if(answer==r);
-	String BobbyTarantino = JOptionPane.showInputDialog("Guess the number");
+String answer = JOptionPane.showInputDialog("Guess the number");
+int answer1 = Integer.parseInt(answer);
+Random gen = new Random ();
+Random factory = new Random();
+int r = factory.nextInt(3);
+System.out.print(r);
+int answer11 = 0;
+if(answer11==r){
+JOptionPane.showMessageDialog(null, "Congrats you guessed the number!");
+	}
 	
-	
-	
-	
+else {
+	JOptionPane.showMessageDialog(null," Sorry guess again");
+}
+	}
 	
 }
-	
-	
-	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
