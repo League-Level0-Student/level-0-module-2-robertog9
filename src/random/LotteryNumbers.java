05395278a14a4c253442542bb7b476e4b5005e9b
96752 <96752@0338S-S-R0B6H0D.sdcs.local>
2018-11-06 14:26:55 -0800

    LotteryNumbersIP

--- src/random/LotteryNumbers.java
+++ src/random/LotteryNumbers.java
@@ -5,11 +5,6 @@ import java.util.Random;
 public class LotteryNumbers {
 public static void main(String[] args) {
 	
-	int guess= Integer.parseInt(null, guess);
-
-	Random gen = new Random ();
-	Random factory = new Random();
-	int r = factory.nextInt(3);
 	System.out.print(r);
 	
 	
