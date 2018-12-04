89bcbe6db47d1c6260bdd8a821ebd061f631897c
96752 <96752@0338S-S-R0B6H0D.sdcs.local>
2018-12-04 14:26:42 -0800

    AreYouHappy,Tester

---
+++ src/AreYouHappy.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+	public static void main(String[] args) {
+
+		
+		String answer = JOptionPane.showInputDialog("Are you happy?");
+ String yes;
+if (answer = "yes") {
+	JOptionPane.showMessageDialog(null, "Keep doing whatever you want");
+
+
+
+}
+}
+}	
+	
+	
